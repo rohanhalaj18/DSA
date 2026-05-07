72b651f6fbb65b3df0c31430a33b328dde3a80d6
rohan halaj <rohanhalaj@gmail.com>
2026-05-07 16:15:27 +0530

    Palindrome

---
+++ arrays/PalindromArray.java
@@ -0,0 +1,18 @@
+public class PalindromArray {
+
+    public static boolean Pailndrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(Pailndrome("madam"));
+    }
+}
