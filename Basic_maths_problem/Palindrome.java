605a91c2884a0666be1fd08f0539b7e681607085
rohan halaj <rohanhalaj@gmail.com>
2026-05-08 11:12:30 +0530

    palindrome number withot array

---
+++ Basic_maths_problem/Palindrome.java
@@ -0,0 +1,20 @@
+
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int n = Math.abs(-1221);
+        int orginal = n;
+        int rev = 0;
+        while (n > 0) {
+            int last_digit = n % 10;
+            rev = rev * 10 + last_digit;
+            n = n / 10;
+        }
+        if (rev == orginal) {
+            System.out.println("it is palindrome");
+            return;
+        } else {
+            System.out.println("not a palindrome");
+        }
+    }
+}
