28b8f4d547c586f4b57acedfcf7e557af2a5aead
rohan halaj <rohanhalaj1@gmail.com>
2026-07-02 21:43:38 +0530

    4th  pattern easy

---
+++ Patterns/fourthpattern.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class fourthpattern {
+    public static void main(String[] args) {
+        for (int i = 0; i <=5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
