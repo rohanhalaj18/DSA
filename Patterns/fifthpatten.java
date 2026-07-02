3ccdc61dd4518bb61579012e7971492bf4023878
rohan halaj <rohanhalaj1@gmail.com>
2026-07-02 21:51:21 +0530

    5th  pattern easy

---
+++ Patterns/fifthpatten.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class fifthpatten {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
