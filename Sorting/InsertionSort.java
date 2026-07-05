080412501a050ea7c65a3ad53dea2581642328a6
rohan halaj <rohanhalaj1@gmail.com>
2026-07-05 20:20:43 +0530

    insertion sort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = { 25, 3, 5, 88, 1, 5 };
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
