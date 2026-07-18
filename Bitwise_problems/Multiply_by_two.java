f7a302066d9b03dbfcdc3ccfcca0c875860eeedd
rohan halaj <rohanhalaj1@gmail.com>
2026-07-18 10:09:26 +0530

    Bitwise problems #2 multiply by 2

---
+++ Bitwise_problems/Multiply_by_two.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Multiply_by_two {
+    public static void main(String[] args) {
+        //left shipt
+        int n = 5;
+        System.out.println(n << 1);
+        
+        // o/p 
+        // 5-> 0101
+        // left shift(<<)-> 1010 (10)
+        // output->10
+    }
+}
