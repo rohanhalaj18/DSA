538e37e2289b1ed9a5be39d6236d4609cc3a4cb8
rohan halaj <rohanhalaj1@gmail.com>
2026-07-18 10:13:42 +0530

    Bitwise problems #3 divide  by 2

---
+++ Bitwise_problems/Divide_by_two.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Divide_by_two {
+    public static void main(String[] args) {
+        
+        //right shipt
+        int n = 6;
+        System.out.println(n >> 1);
+        
+        // output
+        // n=4-> 0100
+        // Right shipt(>>)-> 0010 (2)
+        // ans->2
+    }
+}
