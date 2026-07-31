894ca01c8a7bd203a88cd3cbe51d7467ee2c4c77
rohanhalaj18 <rohanhalaj1@gmail.com>
2026-07-31 12:42:54 +0530

    array  problems #14 Count_Maximum_Consecutive_One

---
+++ Strings/Reverse_vowels.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Reverse_vowels {
+    public static void main(String[] args) {
+        String vowels = "APPEIOU";
+        char[] a = vowels.toCharArray();
+        int left = 0;
+        int right = vowels.length() - 1;
+        while (right > left) {
+            if(a[left]=)
+        }
+        
+        
+    }
+}
---
+++ arrays/Count_Maximum_Consecutive_One.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class Count_Maximum_Consecutive_One {
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 0, 1,0, 1, 1,1 };
+        int count = 0;
+        int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+                
+            } else {
+                count = 0;
+            }
+            max = Math.max(count, max);
+        }
+        System.out.println(max);
+    }
+}
