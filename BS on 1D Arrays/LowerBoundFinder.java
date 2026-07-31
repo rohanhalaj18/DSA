31f2563d60c6e6762877dd23cfd2960b2425a673
rohanhalaj18 <rohanhalaj1@gmail.com>
2026-07-31 20:43:37 +0530

    BS #2 Lower bond

---
+++ BS on 1D Arrays/LowerBoundFinder.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class LowerBoundFinder {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};  // Sorted array
+        int x = 9; // Target value
+        int low = 0; //start index
+        int high = arr.length - 1; //end index
+        int ans = arr.length; //default value if not found
+        
+
+        while (low <= high) {
+            int mid = low + (high-low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
